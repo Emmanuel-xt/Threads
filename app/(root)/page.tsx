95d723aad06a64aa24ba3b8eefd2133236223fe4
Emmanuel-xt <fattmane5@gmail.com>
2024-01-27 23:00:42 +0100

    Clerk Authentication Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <main className="">
-     <h1>Threads</h1>
-    </main>
-  );
+    <>
+      <h1 className="head-text">Home</h1>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+    </>
+  )
 }
