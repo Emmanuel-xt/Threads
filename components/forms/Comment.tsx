ff2a8ae40fbdec152971e4e55affe3960a7bf794
Emmanuel-xt <fattmane5@gmail.com>
2024-04-21 23:57:35 +0100

    comment form / image

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -27,57 +27,64 @@ import { CommentValidation } from "@lib/validations/thread";
 import { createThread } from "@lib/actions/threads.actions";
 
 interface Props {
-    threadId : string;
-    currentUserId : string;
-    currentUserImg : string;
+  threadId: string;
+  currentUserId: string;
+  currentUserImg: string;
 }
-const Comment = ({threadId , currentUserId , currentUserImg} : Props) => {
-    const router = useRouter();
-    const pathname = usePathname();
-  
-    const form = useForm<z.infer<typeof CommentValidation>>({
-      resolver: zodResolver(CommentValidation),
-      defaultValues: {
-        thread: "",
-      },
-    });
-  
-    const onSubmit = async (values : z.infer<typeof CommentValidation>)  => {
+const Comment = ({ threadId, currentUserId, currentUserImg }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm<z.infer<typeof CommentValidation>>({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
     //   await createThread({
     //       text : values.thread,
     //       author:userId , communityId : null , path:pathname
     //   })
     //   router.push('/')
-  
-    };
+  };
   return (
     <Form {...form}>
-    <form
-      className="flex flex-col justify-start gap-10"
-      onSubmit={form.handleSubmit(onSubmit)}
-    >
-      <FormField
-        control={form.control}
-        name="thread"
-        render={({ field }) => (
-          <FormItem className="flex w-full flex-col gap-3">
-            <FormLabel className="text-base-semibold text-light-2">
-              Content
-            </FormLabel>
-            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
-              <Input type="text" placeholder="comment....." className="no-focus text-light-1 outline-none"  {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+      <form className="comment-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="">
+                <Image
+                  src={currentUserImg}
+                  alt="Profile-Image"
+                  width={48}
+                  height={48}
+                  classname="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  type="text"
+                  placeholder="comment....."
+                  className="no-focus text-light-1 outline-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <Button type="submit" className="bg-primary-500">
-        Post Thread
-      </Button>
-    </form>
-  </Form>
-  )
-}
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+};
 
-export default Comment
+export default Comment;
